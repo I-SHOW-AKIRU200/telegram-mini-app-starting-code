2b00a5d55f4d1783810346f131b9640e2aa87a1d
Smart Oghenemine <smartjeremy94@gmail.com>
2024-11-07 21:18:26 +0100

    add dynamic caching

--- app/api/fetch/tasks/route.ts
+++ app/api/fetch/tasks/route.ts
@@ -2,6 +2,8 @@ import { db } from "@/lib/database/firebase";
 import { collection, getDocs, query } from "firebase/firestore";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(): Promise<NextResponse> {
   try {
     const tasksRef = collection(db, "tasks");
--- app/api/fetch/user-details/route.ts
+++ app/api/fetch/user-details/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/database/firebase";
 import { doc, getDoc } from "firebase/firestore";
 import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
 
 export async function POST(request: Request): Promise<NextResponse> {
   try {
